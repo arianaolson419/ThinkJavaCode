9b9eadcf0592685ef838074c622a2c439f165e80
arianaolson419 <ariana.olson@students.olin.edu>
2016-05-10 17:56:11 -0400

    added statements to print date in American and European formats

--- ch02/Data.java
+++ ch02/Data.java
@@ -1,5 +1,20 @@
 public class Data {
-	public static void main(String args[]) {
-	
+	public static void main(String[] args) {
+		// initialize vars and assign values
+
+		String day, month;
+		int date, year;
+		day = "Tuesday";
+		date = 10;
+		month = "May";
+		year = 2016;
+
+		// print the date in standard American format
+		System.out.println("American format:");
+		System.out.println(day + ", " + month + " " + date + ", " + year);
+
+		//print the date in standard European format
+		System.out.println("European format:");
+		System.out.println(day + " " + date + " " + month + " " + year);
 	}
 }
