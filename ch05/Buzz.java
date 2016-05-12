2ef0932da277dad97a07f8afbe0d38825cd2db06
arianaolson419 <ariana.olson@students.olin.edu>
2016-05-12 15:07:16 -0400

    added comments to complete 5.6

--- ch05/Buzz.java
+++ ch05/Buzz.java
@@ -1,22 +1,30 @@
 public class Buzz {
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+    /*4*/    System.out.println(blimp);
+    /*5*/    zippo("ping", -5);
     }
 
     public static void zippo(String quince, int flag) {
         if (flag < 0) {
-            System.out.println(quince + " zoop");
+        /*6*/    System.out.println(quince + " zoop");
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+        /*2*/    System.out.println("ik");
+        /*3*/    baffle(quince);
+            /*7*/System.out.println("boo-wa-ha-ha");
         }
     }
 
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        /*1*/zippo("rattle", 13);
     }
 
 }
+
+/*String blimp = "rattle"*/
+/*
+ik
+rattle
+ping zoop
+boo-wa-ha-ha
+*/
