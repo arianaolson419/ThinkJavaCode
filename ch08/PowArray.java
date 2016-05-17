56e00d1b583ba87e4f591fce3c29dea632bd74c8
arianaolson419 <ariana.olson@students.olin.edu>
2016-05-17 13:08:33 -0400

    completed 8.1

--- ch08/PowArray.java
+++ ch08/PowArray.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class PowArray {
 	public static void main(String[] args) {
 		double[] a = {4, 8, 1, 2};
-		System.out.println(Arrays.toString(powArray(a, 2)));
+		int[] scores = randomArray(10);
+		System.out.println(Arrays.toString(scores));
+		System.out.println(Arrays.toString(histogram(scores, 10)));
 	}
 
 	public static double[] powArray(double[] a, int n) {
@@ -12,5 +15,23 @@ public class PowArray {
 			b[i] = Math.pow(a[i], n);
 		}
 		return b;
+	}
+
+	public static int[] randomArray(int size) {
+		Random random = new Random();
+		int[] a = new int[size];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = random.nextInt(100);
+		}
+		return a;
+	}
+
+	public static int[] histogram(int[] scores, int bins) {
+		int[] counts = new int[bins];
+		for (int score : scores) {
+			counts[score/bins]++;
+		}
+
+		return counts;
 	}
 }
