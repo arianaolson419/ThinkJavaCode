25f296b2ce6714a5d9fb309f7d158ccb973d6494
arianaolson419 <ariana.olson@students.olin.edu>
2016-05-18 09:52:54 -0400

    created for 8.8

---
+++ ch08/RecurseArrays.java
@@ -0,0 +1,16 @@
+public class RecurseArrays {
+	public static void main(String[] args) {
+		
+	}
+
+	public static int maxInRange(int[] array, int lowIndex, int highIndex) {
+		if (lowIndex == highIndex) {
+			return array[highIndex];
+		}
+
+		else {
+			
+		}
+
+	}
+}
